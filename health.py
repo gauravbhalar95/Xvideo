08680bedf157c98244e3b7fae26d9106d1adf1d6
gauravbhalar95 <141895800+gauravbhalar95@users.noreply.github.com>
2024-10-14 10:59:23 +0530

    Update health.py

--- health.py
+++ health.py
@@ -1,12 +1,11 @@
+# health.py
 from flask import Flask
 
-# Create a Flask application instance
 app = Flask(__name__)
 
-# Define a health check route
 @app.route('/health', methods=['GET'])
 def health_check():
     return 'OK', 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)  # Run the app on port 8000
+    app.run(host='0.0.0.0', port=8001)  # Health check runs on port 8001
